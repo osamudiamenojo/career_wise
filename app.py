f11e8e6c56eab141fc63b023d15022720c3f3ba2
Osamudiamen Ojo <victorojo007@gmail.com>
2023-04-26 07:51:41 +0100

    created boiler plate code

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, redirect, url_for, request, session, flash
+from repository import *
+from werkzeug.security import check_password_hash, generate_password_hash
+
+
+app = Flask(__name__)
+app.secret_key = 'CareewiseProjectApplication'
+
+
+
+@app.route('/')
+def index():
+    return "<h1>Hello World</h1>"
+
+if __name__ == '__main__':
+    app.run(debug = True)
+

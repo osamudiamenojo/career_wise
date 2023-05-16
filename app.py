00d4610d3e3163acfa5e41cdb3026304be5c0210
Osamudiamen Ojo <victorojo007@gmail.com>
2023-05-16 19:35:31 +0100

    removed forms.py

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, g, redirect, url_for, request
 from flask_login import login_user, LoginManager, login_required, current_user
-from forms import LoginForm, SignupForm
 from flask_uploads import UploadSet,IMAGES, configure_uploads
 # from repository import CareerManagerDB as Queries
 import os
@@ -8,7 +7,6 @@ from werkzeug.security import check_password_hash, generate_password_hash
 from onet_api import OnetApi as ONetAPI, username, password
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///carrers.db'
 app.secret_key = 'CareewiseProjectApplication'
 app.config['UPLOADED_PHOTOS_DEST'] = os.path.join(os.getcwd(), 'static', 'images')
 photos = UploadSet('photos', IMAGES)
@@ -16,15 +14,6 @@ configure_uploads(app,photos)
 
 api = ONetAPI(username=f"{username}", password=f"{password}")
 
-# queries = Queries()
-
-# login_manager = LoginManager()
-# login_manager.init_app(app)
-# login_manager.login_view = "login"
-
-# @login_manager.user_loader
-# def load_user(user_id):
-#   return queries.session.get(User, int(user_id))
 
 @app.before_request
 def before_request():
@@ -34,41 +23,6 @@ def before_request():
 def log_report(report):
   with open("report.txt", 'a', encoding='utf-8') as f:
         f.write(f'{report}\n')
-
-
-# @app.route('/signup', methods=['GET', 'POST'])
-# def signup():
-#   form = SignupForm()
-#   error = None
-
-#   if form.validate_on_submit():
-#     password_hash = generate_password_hash(form.password.data)
-#     queries.add_user(form.name.data, form.email.data,
-#                             password_hash)
-#     user = queries.find_user_by_email(form.email.data)
-#     # user = User.query.filter_by(email=form.email.data).first()
-#     if user:
-#       login_user(user)
-#     else:
-#       error = "unsuccesful"
-#     return redirect(url_for('dashboard')) 
-#   if form.errors:
-#     log_report('nothing')
-#   return render_template("signup.html", form=form, error=error)
-
-# @app.route('/login', methods=['GET', 'POST'])
-# def login():
-#     error = None
-#     form = LoginForm()
-#     if form.validate_on_submit():
-#         user = queries.find_user_by_email(email=form.email.data)
-#         if user and check_password_hash(user.password, form.password.data):
-#             login_user(user)
-#             return redirect(url_for('dashboard'))
-#         else:
-#             error = "invalid password / user"
-
-#     return render_template("login.html", form=form, error=error)
 
 @app.route("/dashboard", methods=['GET', 'POST'])
 @login_required
@@ -148,13 +102,3 @@ def profiler(start):
   # for num in range 
 
   return render_template('profiler.html',questions)
-
-
-
-
-
-# code = '17-1011.00'
-# occupation = api.get_occupation(code)
-
-
-# log_report(education)
